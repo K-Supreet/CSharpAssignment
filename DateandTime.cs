4d28e69d1fd0bf29a683ed0c7221bd782a38f993
K-Supreet <107865222+K-Supreet@users.noreply.github.com>
2022-08-23 00:14:45 +0530

    Add files via upload

---
+++ DateandTime.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+namespace Assignment
+{
+    internal class prgm
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("The current Date and Time is : ");
+            DateTime now = DateTime.Now;
+            Console.WriteLine(now);
+            Console.ReadLine();
+        }
+    }
+}
