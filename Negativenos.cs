b30ef7f4aa756ebb76af3da8f356757a6b314882
K-Supreet <107865222+K-Supreet@users.noreply.github.com>
2022-08-24 23:57:07 +0530

    Add files via upload

---
+++ Negativenos.cs
@@ -0,0 +1,22 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+namespace ConsoleApp2
+{
+    internal class negativenumbers
+    {
+        public static void Main()
+        {
+            int[] arr = { 60, 25, -21 , 3, -85, 32 };
+            int i, n;
+            n = arr.Length;
+            for (i = 1; i < n; i++)
+            {
+                if (arr[i] < 0)
+                {
+                    Console.Write($"{arr[i]}");
+                }
+
+            }
+        }
+    }
+}
