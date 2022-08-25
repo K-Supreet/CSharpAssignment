7d20dbbb1d0fbce5a90265ea65c77fa6235532ab
K-Supreet <107865222+K-Supreet@users.noreply.github.com>
2022-08-25 19:46:54 +0530

    Add files via upload

---
+++ Swap.cs
@@ -0,0 +1,22 @@
+﻿
+
+namespace ConsoleApp33
+{
+    internal class Swap
+    {
+        public static void Swapnumber(ref int a, ref int b)
+        {
+            a += b;
+            b = a - b;
+            a -= b;
+        }
+        public static void Main()
+        {
+            int a = 30;
+            int b = 40;
+            Console.WriteLine($"Before Swap a={a} b={b}");
+            Swapnumber(ref a, ref b);
+            Console.WriteLine($"After Swap a={a} b={b}");
+        }
+    }
+}
